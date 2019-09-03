617d000be892c2cfc6495fcdc01025e5fcc03377
Michelle <Michelle.cueva@gmail.com>
2019-09-03 15:21:46 -0400

    Adds Custom Cell

---
+++ Concurrency Lab/CountriesTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  CountriesTableViewCell.swift
+//  Concurrency Lab
+//
+//  Created by Michelle Cueva on 9/3/19.
+//  Copyright © 2019 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class CountriesTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var countryImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var capitalLabel: UILabel!
+    
+    @IBOutlet weak var populationLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
